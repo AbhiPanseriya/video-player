e49e2970e1301d8549d0432649aec84a8ae9121b
AbhiPanseriya <abhipanseriya@gmail.com>
2021-06-05 22:55:33 +0530

    fix: fixed a bug in offline support

--- public/sw.js
+++ public/sw.js
@@ -1,16 +1,21 @@
 const CACHE_NAME = 'avplayer_cache';
 const urlsToCache = [
     '/',
-    '/static',
     '/logo-blue.svg',
     'manifest.json'
 ];
+const buildUrls = [
+    '/static/js/2.d13ba113.chunk.js',
+    '/static/js/main.803259b2.chunk.js',
+    '/static/css/main.f89e7a53.chunk.css',
+    '/static/js/runtime-main.19af7ed0.js'
+]
 self.addEventListener('install', function (event) {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(function (cache) {
                 // Open a cache and cache our files
-                return cache.addAll(urlsToCache);
+                return cache.addAll([...urlsToCache, ...buildUrls]);
             })
     );
 });
